1b62f7add176197ce6550e4382506e01fc9c6085
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-07-11 17:55:46 +1000

    Update user.sh

--- user.sh
+++ user.sh
@@ -9,4 +9,10 @@ git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
 makepkg -si --noconfirm
 
+# dotfiles
+cd /tmp
+wget https://raw.githubusercontent.com/Connor-McCartney/deploy-arch-hyprland/refs/heads/main/dotfiles/install.sh
+chmod +x install.sh
+./install.sh
+
 rm /home/connor/user.sh

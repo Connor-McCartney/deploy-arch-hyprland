eab616eeb8057899ed8ddbf3397d53d19af8bf8c
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-07-13 00:19:31 +1000

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -6,10 +6,7 @@ git clone https://github.com/Connor-McCartney/deploy-arch-hyprland
 mv /tmp/deploy-arch-hyprland/dotfiles/.autostart.sh /home/connor
 chmod +x /home/connor/.autostart.sh
 
-mv /tmp/deploy-arch-hyprland/dotfiles/.config/hypr /home/connor/.config
-mv /tmp/deploy-arch-hyprland/dotfiles/.config/alacritty /home/connor/.config
-mv /tmp/deploy-arch-hyprland/dotfiles/.config/waybar /home/connor/.config
-mv /tmp/deploy-arch-hyprland/dotfiles/.config/nvim /home/connor/.config
-
+rm -rf /home/connor/.config
+mv /tmp/deploy-arch-hyprland/dotfiles/.config /home/connor
 
 rm -rf /tmp/deploy-arch-hyprland

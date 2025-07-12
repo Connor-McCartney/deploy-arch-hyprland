37c6999fa1c1f9b82846629dc1552a2e19c32b3d
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-07-13 00:25:42 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -7,7 +7,7 @@ locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "arch" > /etc/hostname
 
-pacman -S --noconfirm hyprland xdg-desktop-portal-hyprland waybar
+pacman -S --noconfirm hyprland xdg-desktop-portal-hyprland waybar wl-clipboard
 pacman -S --noconfirm networkmanager neovim sudo base-devel os-prober grub vlc htop git wget ripgrep fzf bat fd kitty
 systemctl enable NetworkManager
 

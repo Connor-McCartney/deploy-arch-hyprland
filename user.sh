24d749df0d05356ca706e245021b967d9892daaf
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-07-13 00:17:04 +1000

    Update user.sh

--- user.sh
+++ user.sh
@@ -15,7 +15,6 @@ wget https://raw.githubusercontent.com/Connor-McCartney/deploy-arch-hyprland/ref
 chmod +x install.sh
 ./install.sh
 
-paru -S --noconfirm waybar
 paru -S --noconfirm  noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-hack-nerd ttf-comic-mono-git
 paru -S --noconfirm brave-bin discord obsidian flameshot thunar alacritty # sagemath google-chrome
 paru -S --noconfirm gvfs usbutils

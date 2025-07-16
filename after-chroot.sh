82a926170c47293bb6c375a1c63f3c4ee520e1ea
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-07-17 02:15:00 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -8,7 +8,7 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "arch" > /etc/hostname
 
 pacman -S --noconfirm hyprland xdg-desktop-portal-hyprland waybar hyprpaper wl-clipboard wofi grim
-pacman -S --noconfirm networkmanager neovim sudo base-devel os-prober grub vlc htop git wget ripgrep fzf bat fd kitty alacritty thunar
+pacman -S --noconfirm networkmanager wireshark-qt neovim sudo base-devel os-prober grub vlc htop git wget ripgrep fzf bat fd kitty alacritty thunar
 systemctl enable NetworkManager
 pacman -S --noconfirm pulseaudio pavucontrol
 
@@ -35,7 +35,7 @@ printf "[Autologin]\nUser=connor\nSession=hyprland.desktop\n" > /etc/sddm.conf.d
 systemctl enable sddm
 
 
-useradd -m -G users,wheel,audio,video,input -s /bin/bash connor
+useradd -m -G users,wheel,audio,video,input,wireshark -s /bin/bash connor
 printf " \n \n" | passwd
 printf " \n \n" | passwd connor
 echo -e "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) NOPASSWD: ALL\n@includedir /etc/sudoers.d" > /etc/sudoers

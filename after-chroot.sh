a1020261dcd7a9591f79ba6c0413f181e1da8fb3
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-07-17 02:58:38 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -8,7 +8,7 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "arch" > /etc/hostname
 
 pacman -S --noconfirm hyprland xdg-desktop-portal-hyprland waybar hyprpaper wl-clipboard wofi grim
-pacman -S --noconfirm networkmanager wireshark-qt neovim sudo base-devel os-prober grub vlc htop git wget ripgrep fzf bat fd kitty alacritty thunar
+pacman -S --noconfirm networkmanager wireshark-qt neovim sudo base-devel os-prober grub vlc htop git wget ripgrep fzf bat fd kitty alacritty thunar ydotool
 systemctl enable NetworkManager
 pacman -S --noconfirm pulseaudio pavucontrol
 

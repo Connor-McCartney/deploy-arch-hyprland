98fc7717bc1dd3ef32c61b8eb198295ed18f2860
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-07-25 11:27:02 +1000

    Update .switcher.sh

--- dotfiles/.switcher.sh
+++ dotfiles/.switcher.sh
@@ -5,20 +5,33 @@
 hyprctl dispatch layoutmsg focusmaster master
 
 sudo ydotool key 125:0 # windows up
+#sleep 0.1
 sudo ydotool key 1:0 # esc up
+#sleep 0.1
 sudo ydotool key 1:1 # esc down
+#sleep 0.1
 sudo ydotool key 1:0 # esc up
+#sleep 0.1
 sudo ydotool type ':w\n'
+#sleep 0.1
 
-sudo ydotool key 125:1 15:1 125:0 15:0 # switch (win-tab)
+#sudo ydotool key 125:1 15:1 125:0 15:0 # switch (win-tab)
+hyprctl dispatch layoutmsg focusmaster slave
+#sleep 0.1
 
 #sleep 0.1
 
 # ctrl-l
 sudo ydotool key 29:1 
+#sleep 0.1
 sudo ydotool type 'l'
+#sleep 0.1
 sudo ydotool key 29:0
+#sleep 0.1
 
 sudo ydotool key 103:1 103:0 # up
+#sleep 0.1
 sudo ydotool key 28:1 28:0 # enter
-sudo ydotool key 125:1 15:1 125:0 15:0 # switch (win-tab)
+#sleep 0.1
+#sudo ydotool key 125:1 15:1 125:0 15:0 # switch (win-tab)
+hyprctl dispatch layoutmsg focusmaster master

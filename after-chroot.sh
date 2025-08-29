7866fa12181abfe58fc04ba07589180cb156cbd0
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-08-30 00:00:14 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -13,19 +13,19 @@ systemctl enable NetworkManager
 pacman -S --noconfirm pulseaudio pavucontrol
 
 # non-LUKS
-#printf "GRUB_TIMEOUT=1\nGRUB_DISTRIBUTOR=\"Arch\"\nGRUB_CMDLINE_LINUX=\"loglevel=1 nowatchdog nvme_load=YES fsck.mode=skip modprobe.blacklist=iTCO_wdt\"\n" > /etc/default/grub
-#grub-install /dev/sda # BIOS
-##pacman -S efibootmgr --noconfirm; grub-install --target=x86_64-efi --efi-directory=/boot # UEFI
-#grub-mkconfig -o /boot/grub/grub.cfg
+printf "GRUB_TIMEOUT=1\nGRUB_DISTRIBUTOR=\"Arch\"\nGRUB_CMDLINE_LINUX=\"loglevel=1 nowatchdog nvme_load=YES fsck.mode=skip modprobe.blacklist=iTCO_wdt\"\n" > /etc/default/grub
+grub-install /dev/sda # BIOS
+#pacman -S efibootmgr --noconfirm; grub-install --target=x86_64-efi --efi-directory=/boot # UEFI
+grub-mkconfig -o /boot/grub/grub.cfg
 
 # LUKS UEFI
-printf "MODULES=()\nBINARIES=()\nFILES=()\nHOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block encrypt lvm2 filesystems fsck)\n" > /etc/mkinitcpio.conf
-pacman -S --noconfirm thin-provisioning-tools lvm2
-mkinitcpio -p linux
-pacman -S efibootmgr --noconfirm; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-uuid=$(blkid -s UUID -o value /dev/nvme0n1p2)
-printf "GRUB_TIMEOUT=1\nGRUB_DISTRIBUTOR=\"Arch\"\nGRUB_CMDLINE_LINUX=\"loglevel=1 nowatchdog cryptdevice=UUID="$uuid":cryptlvm root=/dev/vg1/root nvme_load=YES fsck.mode=skip modprobe.blacklist=iTCO_wdt\"\n" > /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+#printf "MODULES=()\nBINARIES=()\nFILES=()\nHOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block encrypt lvm2 filesystems fsck)\n" > /etc/mkinitcpio.conf
+#pacman -S --noconfirm thin-provisioning-tools lvm2
+#mkinitcpio -p linux
+#pacman -S efibootmgr --noconfirm; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+#uuid=$(blkid -s UUID -o value /dev/nvme0n1p2)
+#printf "GRUB_TIMEOUT=1\nGRUB_DISTRIBUTOR=\"Arch\"\nGRUB_CMDLINE_LINUX=\"loglevel=1 nowatchdog cryptdevice=UUID="$uuid":cryptlvm root=/dev/vg1/root nvme_load=YES fsck.mode=skip modprobe.blacklist=iTCO_wdt\"\n" > /etc/default/grub
+#grub-mkconfig -o /boot/grub/grub.cfg
 
 
 # sddm auto-login

745b8dbec212db738e4d34524b0de58fa2102b22
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-12-25 06:33:37 -0500

    Update treesitter.lua

--- dotfiles/.config/nvim/lua/connor/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/connor/plugins/treesitter.lua
@@ -1,33 +1,42 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter-context',
-    },
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
     branch = 'main',
     lazy = false,
-    build = ':TSUpdate',
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+
     config = function()
-        local treesitter = require('nvim-treesitter')
-        treesitter.install({
-            "odin",
-            "json",
-            "javascript",
-            "typescript",
-            "tsx",
-            "yaml",
-            "html",
-            "css",
-            "markdown",
-            "markdown_inline",
-            "bash",
-            "lua",
-            "vim",
-            "dockerfile",
-            "c",
-            "cpp",
-            "python",
-            "rust",
-            "cmake",
+        local treesitter = require("nvim-treesitter")
+
+        treesitter.setup({
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            ensure_installed = {
+                "odin",
+                "json",
+                "javascript",
+                "typescript",
+                "tsx",
+                "yaml",
+                "html",
+                "css",
+                "markdown",
+                "markdown_inline",
+                "bash",
+                "lua",
+                "vim",
+                "dockerfile",
+                "c",
+                "cpp",
+                "python",
+                "rust",
+                "cmake",
+            },
         })
     end,
 }
